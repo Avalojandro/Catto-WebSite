6a378a9d131c02c5eaa3f0e773a77560f31db630
avalojandro <avalojandro@gmail.com>
2020-01-08 21:56:59 -0600

    website

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+function copiarAlPortapapeles(id_elemento) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+  }
